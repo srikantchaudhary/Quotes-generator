8567e9788695e98aa233c19f3b118e87a390a9e8
Srikant <chaudharysrikant26@gmail.com>
2024-03-28 14:00:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { quote: ''};
+    componentDidMount() {
+        this.getAdvice();
+    }
+
+    getAdvice = () => {
+        const url = ` https://hindi-quotes.vercel.app/random`;
+        axios.get(url)
+            .then((response) => {
+                const { quote } = response.data;
+                console.log('Successful API request');
+                this.setState({quote});
+            })
+            .catch((error)=>{
+                alert("Error retrieving data");
+            });
+    };
+  render(){
+    return (
+      <div className="App">
+        <div className='card'>
+          <h1 className='heading'>{this.state.quote}</h1>
+          <button onClick={this.getAdvice} className='btn'>Get Advice</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
